4b66acbea98eeb3f19e85885547e1cc9593bbd38
voiceinthedark <darkrisingforce@gmail.com>
2025-09-04 15:37:46 +0300

    preview header setup
    feat: added the preview header
    style: added the styling for the preview header
    style: fix font color

--- src/components/resume/Header.jsx
+++ src/components/resume/Header.jsx
@@ -9,6 +9,9 @@ export default function Header({ data }) {
         <h1>{data.profile.fullname}</h1>
         <h3>{data.profile.speciality}</h3>
       </div>
+      <div className="header-right">
+        <img className="header-photo" src={data.profile.photo} alt="profile photo" />
+      </div>
     </header>
   )
 }
