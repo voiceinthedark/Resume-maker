2c57a6fcf5bffcda207b53af59fe1d8c901ebac0
voiceinthedark <darkrisingforce@gmail.com>
2025-09-05 20:47:58 +0300

    Download cv as pdf
    feat: users now can download the generated cv as pdf
    style: styling to conform the contacts on the resume preview
    build: add jspdf and html2canvas dependencies
    refactor: new set of initial data

--- src/components/header/HeaderDownload.jsx
+++ src/components/header/HeaderDownload.jsx
@@ -1,10 +1,41 @@
 import DownloadIcon from '../icons/DownloadIcon'
 import "../../styles/downloadheader.css"
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+
 export default function HeaderDownload() {
+
+  async function downloadPdf() {
+    try {
+      const doc = new jsPDF('p', 'mm', 'a4')
+      const resume = document.querySelector('.resume-preview')
+
+      const canvas = await html2canvas(resume, {
+        scale: 2,
+        useCORS: true,
+        logging: false,
+      })
+
+      const imgData = canvas.toDataURL('image/png')
+      const imgWidth = 210
+      const imgHeight = (canvas.height * imgWidth) / canvas.width
+
+      doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight)
+
+      doc.save('cv.pdf')
+    } catch (error) {
+      console.error('Failed to download the pdf', error);
+    }
+
+  }
+
   return (
     <div className="header-download">
-      <button className='download-btn'>
-        <DownloadIcon fill='black'/>
+      <button
+        className='download-btn'
+        onClick={downloadPdf}
+      >
+        <DownloadIcon fill='black' />
       </button>
     </div>
   )
--- src/components/utils/sampleData.js
+++ src/components/utils/sampleData.js
@@ -1,44 +1,65 @@
 export default {
   profile: {
-    fullname: 'Eren Yaegar',
-    speciality: 'Titan hunter',
+    fullname: 'Sylvan Prometheus',
+    speciality: 'Software Developer',
     photo: '/1313942.png',
-    bio: 'Seeking revenge and justice for my people',
+    bio: 'Chillout but determined, likes to get things done and crossing a task. Love nature, cats and dogs... And pigs too. Journey before destination',
   },
   links: [
-    { id: 'address', label: 'Address', value: 'Paradis'}, 
+    { id: 'address', label: 'Address', value: 'Tyre, South governate, Lebanon' },
     { id: 'phone', label: 'Phone', value: '(212)883223' },
-    { id: 'email', label: 'Mail', value: 'Eren@wallsoldiers.com' },
-    { id: 'github', label: 'Github', value: 'erenyaegar.github.com' },
-    { id: 'homepage', label: 'HomePage', value: 'www.erenyaegar.com' },
+    { id: 'email', label: 'Mail', value: 'sylvan@daydreaming.com' },
+    { id: 'github', label: 'Github', value: 'sylvan.github.com' },
+    { id: 'homepage', label: 'HomePage', value: 'www.daydreamer.com' },
   ],
   education: [{ // Initialize with default education as done in EducationEdit
     id: 'paradiso',
-    university: 'Paradiso academy',
-    speciality: 'Titan Hunter',
+    university: 'American university of Beirut',
+    speciality: 'Computer and communication Engineer',
+    yearStart: '2001',
+    yearFinish: '2005',
+    description: 'Electrical systems, electronic circuits, fourier analyses, projects in power supply units'
+  }, {
+    id: 'liu',
+    university: 'Lebanese International University',
+    speciality: 'Computer Science',
     yearStart: '2006',
     yearFinish: '2009',
-    description: 'Hunt or be hunted, defend paradiso at all cost'
-  },],
+    description: 'Algorithms and data structures, knowledge base systems, A*, Djikstra problem-solving algorithm. Project in university registration system',
+  }],
   experience: [{
     id: 'exp-1',
-    job: 'Battle of the wall',
-    role: 'Titan Killer',
-    description: 'Destroyed the attacking titans, secured the city',
+    job: 'Smart lb',
+    role: 'Software developer',
+    description: 'Software development on sharepoint using C# as main programming languages, duties included creating widgets that can be plugged and played directly into any sharepoint site',
     yearStart: '2009',
     yearFinish: '2011',
   },],
   skills: [{
     id: 'skill-1',
-    skill: 'Killing',
+    skill: 'javascript',
+  }, {
+    id: 'skill2',
+    skill: 'react',
+  }, {
+    id: 'skill-3',
+    skill: 'vue'
   },{
-      id: 'skill-2',
-      skill: 'Transform into titan'
+      id: 'skill4',
+      skill: 'css'
+    },{
+      id: 'skill5',
+      skill: 'python'
     }],
   languages: [{
     id: 'lang-1',
     language: 'English',
   },
-    {id: 'lang-2', 
-      language: 'Japanese'}],
+  {
+    id: 'lang-2',
+    language: 'French'
+  },{
+      id: 'lang3',
+      language: 'Arabic'
+    }],
 }
