ca57d2d29e0a3782690369541ed0049a6a928e90
voiceinthedark <darkrisingforce@gmail.com>
2025-09-09 11:31:52 +0300

    education component addition
    feat(education): add a date icon

--- src/components/resume/classic/ClassicEducation.jsx
+++ src/components/resume/classic/ClassicEducation.jsx
@@ -1,4 +1,5 @@
 import "../../../styles/classiceducation.css"
+import CalendarIcon from "../../icons/CalendarIcon"
 export default function ClassicEducation({ data }) {
   return (
     <section className="classic-education">
@@ -9,7 +10,9 @@ export default function ClassicEducation({ data }) {
           <div className="classic-education-item">
             <h3>{educ.university}</h3>
             <h4>{educ.speciality}</h4>
-            <span className="education-date">{educ.yearStart} - {educ.yearFinish}</span>
+            <span className="education-date">
+              <CalendarIcon width="15" height="15" fill='black'/>
+              {educ.yearStart} - {educ.yearFinish}</span>
             <p>{educ.description}</p>
           </div>
         )
